4db292918d6b3ccd7c603511f59c89718538ceb5
Sudipto <sudipto@iMagine.local>
2018-11-17 16:35:37 -0500

    Initial Changes

---
+++ src/main/java/com/grayash/manage/app/GrayashAppmanageApplication.java
@@ -0,0 +1,12 @@
+package com.grayash.manage.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayashAppmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrayashAppmanageApplication.class, args);
+	}
+}
