81759f68e407579d98d43ea9801203f1af766c32
Sudipto Aich <sudipto.aich.osv@fedex.com>
2019-01-24 15:40:44 -0600

    Update GrayashAppmanageApplication.java

--- src/main/java/com/grayash/manage/app/GrayashAppmanageApplication.java
+++ src/main/java/com/grayash/manage/app/GrayashAppmanageApplication.java
@@ -1,10 +1,13 @@
 package com.grayash.manage.app;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import com.grayash.auditactivity.config.EnableAuditActivity;
 import com.grayash.security.EnableGrayashSecurity;
@@ -37,4 +40,15 @@ public class GrayashAppmanageApplication {
                 .paths(PathSelectors.any())
                 .build();
     }
+	
+	 @Bean
+	    public Executor asyncExecutor() {
+	        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+	        executor.setCorePoolSize(5);
+	        executor.setMaxPoolSize(10);
+	        executor.setQueueCapacity(500);
+	        executor.setThreadNamePrefix("PoolCarThread-");
+	        executor.initialize();
+	        return executor;
+	    }
 }
